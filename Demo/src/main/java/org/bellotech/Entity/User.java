9e6e6129e77512807273d18e6810c8acc8c16035
bello1470 <126983812+bello1470@users.noreply.github.com>
2023-11-17 08:45:14 +0000

    3 commits

--- Demo/src/main/java/org/bellotech/Entity/User.java
+++ Demo/src/main/java/org/bellotech/Entity/User.java
@@ -1,5 +1,47 @@
 package org.bellotech.Entity;
 
 public class User {
+	
+	private int user_id;
+	private String username;
+	private String projectName;
+	private String project;
+	
 
+	
+	public User(int user_id, String username, String projectName, String project) {
+		this.user_id = user_id;
+		this.username = username;
+		this.projectName = projectName;
+		this.project = project;
+	}
+	
+	
+	public int getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(int user_id) {
+		this.user_id = user_id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getProjectName() {
+		return projectName;
+	}
+	public void setProjectName(String projectName) {
+		this.projectName = projectName;
+	}
+	public String getProject() {
+		return project;
+	}
+	public void setProject(String project) {
+		this.project = project;
+	}
+
+	
+	
 }
--- Demo/src/main/java/org/bellotech/model/UserModel.java
+++ Demo/src/main/java/org/bellotech/model/UserModel.java
@@ -1,5 +1,55 @@
 package org.bellotech.model;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bellotech.DatabaseConfig;
+import org.bellotech.Entity.User;
+
+
 public class UserModel {
+	
+	
+	public List<User> listuser(){
+		
+		List<User> listusers = new ArrayList<>();
+		
+		
+		//connect the object 
+		Connection connect = DatabaseConfig.getConnection();
+		Statement statement = null;
+		ResultSet rs = null;
+		
+		//create db 
+		
+		String query = "select* from user ";
+		
+		try {
+			statement = connect.createStatement();
+			//Execution of the Statement 
+			
+			rs = statement.executeQuery(query);
+			while(rs.next());
+			listusers.add(new User(rs.getInt("user_id"), rs.getString("username"), rs.getString("projectName"), rs.getString("project")));
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		return listusers;
+		
+	}
 
 }
